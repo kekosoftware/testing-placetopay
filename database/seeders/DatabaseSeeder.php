e43f420a7de75cb37045bdadaa5e8003238140d9
Ariel Coronel <ariel256@gmail.com>
2022-09-15 06:05:39 -0300

    Added factories and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProductSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call([
+            ProductSeeder::class,
+        ]);
+
+        User::factory()->create([
+		    'name' => 'Gustavo Ariel',
+            'email' => 'ariel@gmail.com',
+            'mobile' => '123456789',
+            'email_verified_at' => now(),
+            'password' => bcrypt('Gus@123'), // password
+            'remember_token' => Str::random(10),
+		]);
     }
 }
